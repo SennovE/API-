6787ed90706c47dd65e3b8d4e27ce3ad4f34e6bb
Egor Sennov <esennov2005@gmail.com>
2024-07-16 19:14:35 +0300

    Change get method to post

--- text_analyser/endpoints/language.py
+++ text_analyser/endpoints/language.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Query
+from typing import Annotated
+
+from fastapi import APIRouter, Body
 from starlette import status
 
 from text_analyser.utils import define_language, run_in_thread
@@ -6,16 +8,19 @@ from text_analyser.utils import define_language, run_in_thread
 api_router = APIRouter()
 
 
-@api_router.get(
+@api_router.post(
     "/api/v1/language",
     status_code=status.HTTP_200_OK,
     response_model=str,
 )
 async def define_language_of_the_text(
-    text: str = Query(
-        ...,
-        description="Determines the language of the given text, better in usage with long texts",
-    )
+    text: Annotated[
+        str,
+        Body(
+            embed=True,
+            description="Determines the language of the given text, better in usage with long texts",
+        ),
+    ]
 ):
     tonalities = await run_in_thread(define_language, text)
     return tonalities
--- text_analyser/endpoints/tonality.py
+++ text_analyser/endpoints/tonality.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Query
+from typing import Annotated
+
+from fastapi import APIRouter, Body
 from starlette import status
 
 from text_analyser.schemas import SentenceWithTone, Tonality
@@ -11,25 +13,30 @@ from text_analyser.utils import (
 api_router = APIRouter()
 
 
-@api_router.get(
+@api_router.post(
     "/api/v1/text_tonality",
     status_code=status.HTTP_200_OK,
     response_model=Tonality,
 )
 async def define_text_tonality(
-    text: str = Query(..., description="Determines the tone of the entire text")
+    text: Annotated[
+        str, Body(embed=True, description="Determines the tone of the entire text.")
+    ]
 ):
     tonalities = await run_in_thread(define_tonality, text)
     return tonalities
 
 
-@api_router.get(
+@api_router.post(
     "/api/v1/sentences_tonality",
     status_code=status.HTTP_200_OK,
     response_model=list[SentenceWithTone],
 )
 async def define_sentences_tonality(
-    text: str = Query(..., description="Determines the tone of individual sentences.")
+    text: Annotated[
+        str,
+        Body(embed=True, description="Determines the tone of individual sentences."),
+    ]
 ):
     tonalities = await run_in_thread(define_tonality_in_sentences, text)
     return tonalities
